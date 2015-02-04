5192631dbf7e604951620675a23c90c3fe5e33b3
Alicja Bandasiewicz <alicja9301@gmail.com>
2015-02-04 01:20:34 +0100

    obsługa logowania

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   rescue_from CanCan::AccessDenied do |exception|
-    flash[:error] = exception.message
-    redirect_to root_url
-  end  
+    redirect_to main_app.root_url, :alert => exception.message
+  end
 end
