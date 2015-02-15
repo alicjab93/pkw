e8e0e7012d78eef8c4646819bafc1a6c74063003
Alicja Bandasiewicz <alicja9301@gmail.com>
2015-02-15 23:32:57 +0100

    raporty

--- app/controllers/report/provinces_controller.rb
+++ app/controllers/report/provinces_controller.rb
@@ -1,10 +1,28 @@
 module Report
-class ProvincesController < BaseController
+  class ProvincesController < BaseController
+    before_action :authenticate_user!  
+    before_action :set_province, only: [:show]
+    load_and_authorize_resource
 
-  def index
-    current_user.inspect
-    @provinces = Province.all
-  end  
-  
-end
+    def index
+      @provinces = Province.all
+      @constituencies = Constituency.all
+      @valid_votes = ValidVote.all
+      @invalid_votes = InvalidVote.all
+      @committees = Committee.all
+    end  
+
+    def show
+  #    @committees = @province.committees
+    end  
+
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_province
+        @province = Province.find(params[:id])
+        @valid_votes_sum = @province.valid_votes.sum('quantity')
+        @invalid_votes_sum = @province.invalid_votes.sum('quantity')
+      end
+    end
+    
 end
--- app/models/province.rb
+++ app/models/province.rb
@@ -1,4 +1,6 @@
 class Province < ActiveRecord::Base
   has_and_belongs_to_many :committees
+  has_many :valid_votes, through: :constituencies
+  has_many :invalid_votes, through: :constituencies
   has_many :constituencies
 end
