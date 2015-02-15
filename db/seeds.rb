273bb33ffb9b355867b47d9c85bc2fb0f7e9d140
Alicja Bandasiewicz <alicja9301@gmail.com>
2015-02-15 05:29:53 +0100

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@
 Reason.find_or_create_by(label: 'Wielokrotny wybór')
 Reason.find_or_create_by(label: 'Brak wyboru')
 
+# województwa
 @province = Province.find_or_create_by(id: 1, name: 'dolnośląskie')
-
 @province = Province.find_or_create_by(id: 2, name: 'kujawsko-pomorskie')
 @province = Province.find_or_create_by(id: 3, name: 'lubelskie')
 @province = Province.find_or_create_by(id: 4, name: 'lubuskie')
@@ -27,11 +27,44 @@ Reason.find_or_create_by(label: 'Brak wyboru')
 @province = Province.find_or_create_by(id: 15, name: 'wielkopolskie')
 @province = Province.find_or_create_by(id: 16, name: 'zachodniopomorskie')
 
+# okręgi wyborcze
 require 'csv'    
-
 csv_text = File.read(Rails.root.join('db', 'seeds', 'constituency.csv'))
 csv = CSV.parse(csv_text, :headers => true)
 csv.each do |row|
   #Constituency.find_or_create_by(number: 1, voters:503466, ballots: 165870, mandates: 8, province_id: @province.id)
   Constituency.find_or_create_by(row.to_hash)
+end
+
+# komitety
+Committee.find_or_create_by(id: 1, name: 'Polskie Stronnictwo Ludowe')
+Committee.find_or_create_by(id: 2, name: 'Demokracja Bezpośrednia')
+Committee.find_or_create_by(id: 3, name: 'Prawo i Sprawiedliwość')
+Committee.find_or_create_by(id: 4, name: 'Platforma Obywatelska')
+Committee.find_or_create_by(id: 5, name: 'Ruch Narodowy')
+Committee.find_or_create_by(id: 6, name: 'SLD Lewica Razem')
+Committee.find_or_create_by(id: 7, name: 'Nowa Prawica')
+Committee.find_or_create_by(id: 8, name: 'Bezpartyjni Samorządowcy')
+
+# przypisanie komitetów do województw
+@provinces = Province.all
+committees = Committee.all
+
+@provinces.each do |province|
+  if(province.committees.count == 0)
+    puts 'przypisuję losowe komitety do województwa'
+    province.committees << committees.shuffle[0..3]
+  end
+end
+
+# glosy
+Constituency.all.each do |constituency|
+  iv = InvalidVote.find_or_create_by(constituency_id: constituency.id, reason_id: 1)
+  iv.update_attribute(:quantity, 1 + Random.rand(20))
+  iv = InvalidVote.find_or_create_by(constituency_id: constituency.id, reason_id: 2)
+  iv.update_attribute(:quantity, 1 + Random.rand(20))
+  constituency.committees.each do |committee|
+    v = ValidVote.find_or_create_by(constituency_id: constituency.id, committee_id: committee.id)
+    v.update_attribute(:quantity, Random.rand(1500))
+  end
 end
