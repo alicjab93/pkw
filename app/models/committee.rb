58fd5e8a4c8ad205cede4305618ced930e11f0a6
Alicja Bandasiewicz <alicja9301@gmail.com>
2015-02-22 18:59:11 +0100

    komitet - unikalna nazwa

--- app/models/committee.rb
+++ app/models/committee.rb
@@ -2,4 +2,5 @@ class Committee < ActiveRecord::Base
   has_and_belongs_to_many :provinces
 #  has_many :constituencies, through: :provinces
   has_many :valid_votes#, through: :constituencies
+  validates_uniqueness_of :name
 end
